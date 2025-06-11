b03673f59bc4f9be0654819ad923f331769e2689
138siscog <siscog138@gmail.com>
2025-06-10 21:14:14 -0700

    Update zipy-v0.02.py
    V0.02 Uploaded,
    Features Added 
    1. option screen 
    2. Decompression Feature
    
    Still need to fix some bugs in the loop (see readmev2)

--- zipy-v0.01.py
+++ zipy-v0.01.py
@@ -1,18 +1,39 @@
 from zipfile import ZipFile
 
-files = []
+print("Welcome to Zipy, select what you would like to do:")
+print("1: Compress a file")
+print("2: Decompress a file")
+print("3: Exit")
 
-while True:
-    toZip = input("What file would you like to Zip?: ")
-    files.append(toZip)
+selection = int(input("Make a selection: 1, 2, or 3:"))
 
-    with ZipFile(f'{toZip}.zip', 'w') as zipf:
-        for file in files:
-            zipf.write(file)
-            print(f"Added {file} to zip")
-    another = input("Would you like to zip anopther file? (Y/N)").upper()
-    if  another != "Y":
-        break
-    else:
-        files = files = []
-        continue
+match selection:
+    case 1:
+        files = []
+
+        while True:
+            toZip = input("What file would you like to Zip?: ")
+            files.append(toZip)
+
+            with ZipFile(f'{toZip}.zip', 'w') as zipf:
+                for file in files:
+                    zipf.write(file)
+                print(f"Added {file} to zip")
+            another = input("Would you like to zip another file? (Y/N)").upper()
+            if  another != "Y":
+                break
+            else:
+                files = files = []
+                continue
+
+    case 2:
+
+        zip_path = input('Enter the name of your Zip File (example.zip): ')  
+        extract_path = '.'        
+
+        with ZipFile(zip_path, 'r') as zip_ref:
+            zip_ref.extractall(extract_path)
+            print(f"Extracted all contents of {zip_path} to {extract_path}")
+
+    case 3:
+        exit()
